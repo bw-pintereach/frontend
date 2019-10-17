4cdce68ce19e81a8f0525709c024a762cf2dcbb0
john <john@spinningplates.org>
2019-10-16 20:12:55 -0700

    Dashboard and Navigation components
    Co-authored-by: Dee Barizo <deebarizo@gmail.com>

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
