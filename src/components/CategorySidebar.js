1c11471698134a6ea15ee06e5814ecffaa255105
Dee Barizo <deebarizo@gmail.com>
2019-10-18 20:54:41 -0500

    Add html and state for category sidebar
    Co-authored-by: jchaing <john@spinningplates.org>

---
+++ src/components/CategorySidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function CategorySidebar(props) {
+  const { category, handleCategoryClick } = props;
+  console.log("handleCategoryClick", handleCategoryClick);
+
+  return (
+    <>
+      <p>Select a category:</p>
+
+      <div>
+        <input
+          type="radio"
+          id="all"
+          name="category"
+          value="all"
+          defaultChecked={category === "all" ? true : false}
+          onChange={handleCategoryClick("all")}
+        />
+        <label htmlFor="all">All</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="mobileGame"
+          name="category"
+          value="mobileGame"
+          defaultChecked={category === "mobileGame" ? true : false}
+          onChange={handleCategoryClick("mobileGame")}
+        />
+        <label htmlFor="mobileGame">Mobile Game</label>
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          id="computerGame"
+          name="category"
+          value="computerGame"
+          defaultChecked={category === "computerGame" ? true : false}
+          onChange={handleCategoryClick("computerGame")}
+        />
+        <label htmlFor="computerGame">Computer Game</label>
+      </div>
+    </>
+  );
+}
