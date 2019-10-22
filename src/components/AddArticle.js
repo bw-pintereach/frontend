f5ad06ece3f3d348d37a8731af4116e818d4466f
john <john@spinningplates.org>
2019-10-22 11:53:13 -0700

    Login redirect to dashboard, article POST to API
    Co-authored-by: deebarizo <deebarizo@gmail.com>

--- src/components/AddArticle.js
+++ src/components/AddArticle.js
@@ -1,29 +1,55 @@
-import React, { useState } from 'react';
-import { axiosWithAuth } from "../data/axiosAuth";
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import Navigation from './Navigation';
+import axiosWithAuth from '../data/axiosWithAuth';
 
-
-const initialArticle = {
-    title: "",
-    summary: "",
-    link: "",
-    image: img,
-    category: "", 
+const AddArticle = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Add Article</h1>
+      <Form>
+        <Field type="text" name="id" placeholder="ID" />
+        <Field type="text" name="title" placeholder="Title" />
+        <Field type="text" name="link" placeholder="Link" />
+        <Field type="text" name="image" placeholder="Image" />
+        <Field type="text" name="category" placeholder="Category" />
+        <Field type="text" name="summary" placeholder="Summary" />
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
 };
 
+const FormikAddArticle = withFormik({
+  mapPropsToValues({ id, title, link, image, category, summary }) {
+    return {
+      id: id || '',
+      title: title || '',
+      link: link || '',
+      image: image || '',
+      category: category || '',
+      summary: summary || ''
+    };
+  },
 
-const ArticleList = ({ articles, updateArticles }) => {
-    console.log(articles);
-    const [edit, setEdit] = useState(false);
-    const [articleToEdit, setArticleToEdit] = useState(initialArticle);
+  handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
+    axiosWithAuth()
+      .post(
+        'https://cors-anywhere.herokuapp.com/https://pintreachbackend.herokuapp.com/api/articles/',
+        values
+      )
+      .then(res => {
+        setStatus(res.data);
+        resetForm();
+        setSubmitting(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setSubmitting(false);
+      });
+  }
+})(AddArticle);
 
-
-const editArticle = article => {
-    setEdit(true);
-    setArticleToEdit(color);
-};
-const saveEdit = e => {
-    e.preventDefault();
-};
-
-
-}
+export default FormikAddArticle;
